28bdae0e9bc52cc95f49cd28b389907422eb04f9
Hamid Mehmood Akram <hamid@airlifttech.com>
2023-01-15 14:50:37 +0500

    header + some tweaks on the main component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className="py-3">
         <Container>
           <h1>Welcome to Eddi Store</h1>
         </Container>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,23 @@
 import React from "react";
 
+import {Navbar, Container, Nav} from 'react-bootstrap'
+
 const Header = () => {
   return (
-    <>
-      <header>header</header>
-    </>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+          <Container>
+            <Navbar.Brand href="/">Eddi Store</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link href="/cart"><i className='fas fa-shopping-cart'/>Cart</Nav.Link>
+                <Nav.Link href="/login"><i className='fas fa-user'/>Sign in</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
   );
 };
 

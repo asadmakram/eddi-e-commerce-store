6a6705865da645508e68cdbe08adea3615b1cd9b
Asad Akram <hamid@airlifttech.com>
2023-01-18 20:25:58 +0500

    integrated mongodb atlas with our system

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import colors from 'colors'
+
+const connectDB = async () => {
+  try {
+    console.log(process.env.MONGO_URI)
+    const dbConnection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true
+    });
+
+    console.log(`MongoDB Connected ${dbConnection.connection.host}`);
+  } catch (error) {
+    console.error(
+      `Error occurred while connecting with MongoDb. Error: ${error.message}`
+    );
+
+    process.exit(1); // exit with failure
+  }
+};
+
+
+export default connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express"
+import products  from "./data/products.js"
+import env from 'dotenv'
 
+import connectDB from './config/db.js'
+
+env.config()
+connectDB();
 const app = express();
 app.get("/", (req, res) => {
   res.send("Api is running..");
@@ -15,6 +20,7 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5100, (req, res) => {
-  console.log("server is listening at 5100");
+const port = process.env.PORT || 5100;
+app.listen(port, (req, res) => {
+  console.log(`server is running in ${process.env.NODE_ENV} mode at ${port}`);
 });
